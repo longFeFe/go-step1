a29da658e8713fb5a0728e6add28a9b22a7904a9
CodeMe-PC <c_timemachine@163.com>
2020-08-11 00:09:26 +0800

    format

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ import (
 
 //http://127.0.0.1/download?key=https://dl.google.com/go/go1.14.7.windows-386.msi
 func main() {
-    http.HandleFunc("/hello", api.Hello)
+	http.HandleFunc("/hello", api.Hello)
 	http.HandleFunc("/headers", api.Headers)
 	http.HandleFunc("/download", api.Download)
 	log.Println("start listening...")
-    http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":80", nil)
 }
